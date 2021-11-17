a8a2c12d9c312f83fd90c4a048f1336c302b75b7
Guillaume Vagner <guillaume.vagner@gmail.com>
2021-11-18 00:19:20 +0100

    :zap: Use thread to handle flag updates

--- pixelToPicture.py
+++ pixelToPicture.py
@@ -2,24 +2,30 @@ from math import sqrt, floor
 import numpy as np
 import requests
 from random import random
-import time
+from time import sleep
+from datetime import datetime
+import threading
 
 from kiwi import kiwi, starting_x, starting_y
 from creds import USERNAME, PASSWORD
 
 
-FLAG_API = 'https://api-flag.fouloscopie.com/flag'
+GET_FLAG_URL = 'https://api-flag.fouloscopie.com/flag'
+FOULOSCOPIE_LOGIN_URL = 'https://api.fouloscopie.com/auth/login'
+GET_USER_INFO_URL = 'https://admin.fouloscopie.com/users/me'
+UPDATE_PIXEL_URL = 'https://api-flag.fouloscopie.com/pixel'
+GET_FLAG_UPDATES_URL = 'https://api-flag.fouloscopie.com/flag/after'
 
 
 def get_pixels_with_id():
-    flag_data = requests.get(FLAG_API).json()
+    flag_data = requests.get(GET_FLAG_URL).json()
     return (
         list(map(lambda datum: (datum['hexColor']), flag_data)),
         list(map(lambda datum: (datum['entityId']), flag_data)))
 
 
 def get_diag(pixels):
-    return floor(sqrt(len(pixels) / 2))
+    return floor(sqrt(len(pixels) / 2)) + 1
 
 
 def hex_to_pixel(hex_value):
@@ -30,28 +36,45 @@ def hex_to_pixel(hex_value):
             int(hex_value[5:7], 16),
         ])
     except:
-        print(f'Error parsing hexadecimal value {hex_value}.')
+        if hex_value != None:
+            print(f'Error parsing hexadecimal value {hex_value}.')
         return np.zeros((3))
 
 
 def get_token():
     login_response = requests.post(
-        'https://api.fouloscopie.com/auth/login',
+        FOULOSCOPIE_LOGIN_URL,
         json={'email': USERNAME, 'password': PASSWORD}).json()
     access_token = login_response['access_token']
 
     user_response = requests.get(
-        'https://admin.fouloscopie.com/users/me',
+        GET_USER_INFO_URL,
         headers={'Authorization': f'Bearer {access_token}'}).json()
     fouloscopie_token = user_response['data']['token']
     return fouloscopie_token
 
 
 def update_pixel(pixel_id, color, fouloscopie_token):
-    print(requests.put(
-        'https://api-flag.fouloscopie.com/pixel',
-        json={'hexColor': color, 'pixelId': pixel_id},
-        headers={'Authorization': fouloscopie_token}).json())
+    while True:
+        response = requests.put(
+            UPDATE_PIXEL_URL,
+            json={'hexColor': color, 'pixelId': pixel_id},
+            headers={'Authorization': fouloscopie_token})
+        content = response.json()
+        status_code = response.status_code
+
+        if status_code == 200:
+            return
+        if status_code == 429:
+            time_to_wait = content['retryAfter'] / 1000 + 10 * random()
+            print('Too short! Next try in {0:.2f}s'.format(time_to_wait))
+            sleep(time_to_wait)
+
+
+def get_index_or_none(list, index):
+    if index < len(list):
+        return list[index]
+    return None
 
 
 def get_full_flag_with_id():
@@ -61,40 +84,72 @@ def get_full_flag_with_id():
     full_flag = np.zeros((2 * totalDiag, totalDiag, 3), dtype=np.uint8)
     full_flag_pixel_ids = np.zeros((2 * totalDiag, totalDiag), dtype=object)
 
-    full_flag[0, 0] = hex_to_pixel(pixels[0])
-    full_flag_pixel_ids[0, 0] = pixels_id[0]
-    full_flag[1, 0] = hex_to_pixel(pixels[1])
-    full_flag_pixel_ids[1, 0] = pixels_id[1]
+    def add_pixel_to_flag(x, y, current_pix):
+        full_flag[x, y] = hex_to_pixel(get_index_or_none(pixels, current_pix))
+        full_flag_pixel_ids[x, y] = get_index_or_none(pixels_id, current_pix)
+
+    add_pixel_to_flag(0, 0, 0)
+    add_pixel_to_flag(1, 0, 1)
     currentPix = 2
 
     for diag in range(1, totalDiag):
         for x in range(2 * diag):
-            full_flag[x, diag] = hex_to_pixel(pixels[currentPix])
-            full_flag_pixel_ids[x, diag] = pixels_id[currentPix]
+            add_pixel_to_flag(x, diag, currentPix)
             currentPix += 1
 
         for y in range(diag + 1):
-            full_flag[2 * diag, y] = hex_to_pixel(pixels[currentPix])
-            full_flag_pixel_ids[2 * diag, y] = pixels_id[currentPix]
+            add_pixel_to_flag(2 * diag, y, currentPix)
             currentPix += 1
 
         for y in range(diag + 1):
-            full_flag[2 * diag + 1, y] = hex_to_pixel(pixels[currentPix])
-            full_flag_pixel_ids[2 * diag + 1, y] = pixels_id[currentPix]
+            add_pixel_to_flag(2 * diag + 1, y, currentPix)
             currentPix += 1
 
     return full_flag, full_flag_pixel_ids
 
 
-if __name__ == '__main__':
-    token = get_token()
+def get_last_updates(last_update_ts):
+    return requests.get(
+        f'{GET_FLAG_UPDATES_URL}/{last_update_ts}').json()
 
+
+def get_datetime():
+    return datetime.utcnow().isoformat()[:-3] + 'Z'
+
+
+def update_flag_thread_function(full_flag, full_flag_pixel_ids):
+    print('[UPDATE FLAG] Starting thread')
+
+    last_update_ts = get_datetime()
+    while True:
+        sleep(30)
+        last_updates = get_last_updates(last_update_ts)
+        for update in last_updates:
+            pixel_id = update['entityId']
+            new_color = update['hexColor']
+            flag_index = update['indexInFlag']
+
+            where_result = np.where(full_flag_pixel_ids == pixel_id)
+            if len(where_result[0]) == 0:
+                print('[UPDATE FLAG] Pixel outside initial flag', flag_index)
+                continue
+
+            coords = (where_result[0][0], where_result[1][0])
+            full_flag[coords] = hex_to_pixel(new_color)
+
+        print(f'[UPDATE FLAG] Updated {len(last_updates)} pixels.')
+        last_update_ts = get_datetime()
+
+
+def main_thread_function(full_flag, full_flag_pixel_ids):
+    print('[MAIN] Starting thread')
+
+    token = get_token()
     time_to_wait = 0
 
     while True:
-        print('Next execution in {0:.2f}s'.format(time_to_wait))
-        time.sleep(time_to_wait)
-        full_flag, full_flag_pixel_ids = get_full_flag_with_id()
+        print('[MAIN] Next execution in {0:.2f}s'.format(time_to_wait))
+        sleep(time_to_wait)
 
         clipped_flag = full_flag[
             starting_x:starting_x + len(kiwi),
@@ -108,12 +163,37 @@ if __name__ == '__main__':
                         abs(clipped_flag[x, y] - hex_to_pixel(pixel))) // 3
 
         biggest_diff = np.where(diff == np.amax(diff))
+        coord_x = biggest_diff[0][0]
+        coord_y = biggest_diff[1][0]
         pixel_to_change = full_flag_pixel_ids[
-            starting_x + biggest_diff[0][0],
-            starting_y + biggest_diff[1][0]]
-        new_color = kiwi[biggest_diff[0][0]][biggest_diff[1][0]]
-        print(pixel_to_change, new_color)
+            starting_x + coord_x,
+            starting_y + coord_y]
+        new_color = kiwi[coord_x][coord_y]
 
         update_pixel(pixel_to_change, new_color, token)
+        full_flag[coord_x, coord_y] = hex_to_pixel(new_color)
+        print(f'[MAIN] Updated {pixel_to_change} with {new_color}')
 
         time_to_wait = 120 + 30 * random()
+
+
+if __name__ == '__main__':
+    full_flag, full_flag_pixel_ids = get_full_flag_with_id()
+
+    main_thread = threading.Thread(
+        target=main_thread_function,
+        args=(full_flag, full_flag_pixel_ids),
+        daemon=True
+    )
+
+    update_thread = threading.Thread(
+        target=update_flag_thread_function,
+        args=(full_flag, full_flag_pixel_ids),
+        daemon=True
+    )
+
+    main_thread.start()
+    update_thread.start()
+
+    update_thread.join()
+    main_thread.join()
