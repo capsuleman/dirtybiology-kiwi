1b851076f22fc268541a96bc0d247e4bc7a673f1
Guillaume Vagner <guillaume.vagner@gmail.com>
2021-11-19 01:27:48 +0100

    :bento: Update kiwi

--- kiwi.py
+++ kiwi.py
@@ -1,40 +1,41 @@
-# https://docs.google.com/spreadsheets/d/1Y3u-ph40jMTpmHqJnsdQnnP6Z2J5CJ0-KBiwkuI8gSQ/edit#gid=0
-B0 = "#7E4B15"  # Brown skin
-G0 = "#ACDD6F"  # Green
-G1 = "#2AB41D"  # Dark green
-G2 = "#28D30E"  # Green center
-B1 = "#000000"  # Black seed
+# https://docs.google.com/spreadsheets/d/1NP9f5eyTcv2qx6pi-RXUEiFgG6I4yeJnjhrc6qLVFo8/edit#gid=0
+
+
+G0 = "#749617"
+G1 = "#c9eb63"
+G2 = "#c1cf77"
+G3 = "#f3f09c"
+
+B0 = "#000000"
+
+B1 = "#795614"
+B2 = "#8d6116"
+B3 = "#9c6c06"
+B4 = "#b68d35"
+
 N0 = None  # Transparent
 
-starting_x = 160
-starting_y = 42
+starting_x = 170
+starting_y = 60
 
 kiwi = [
-[N0, N0, N0, N0, N0, N0, N0, N0, N0, N0, B0, B0, B0, B0, B0, B0, B0, B0, B0, N0, N0, N0, N0, N0, N0, N0, N0, N0],
-[N0, N0, N0, N0, N0, N0, N0, B0, B0, B0, G2, G2, G2, G2, G2, G2, G2, G2, G2, B0, B0, N0, N0, N0, N0, N0, N0, N0],
-[N0, N0, N0, N0, N0, B0, B0, B0, G2, G2, G2, G1, G1, G1, G1, G1, G1, G1, G1, G2, G2, B0, B0, N0, N0, N0, N0, N0],
-[N0, N0, N0, N0, B0, B0, G2, G2, G1, G1, G1, G1, G1, G1, G1, G1, G1, G1, G1, G1, G1, G2, G2, B0, N0, N0, N0, N0],
-[N0, N0, N0, B0, B0, G2, G1, G1, G1, G1, G1, G1, G1, G1, G1, G1, G1, G1, G1, G1, G1, G1, G1, G2, B0, N0, N0, N0],
-[N0, N0, N0, B0, B0, G2, G1, G1, G1, G1, G1, G1, G1, G1, G1, G1, G1, G1, G1, G1, G1, G1, G1, G2, B0, N0, N0, N0],
-[N0, N0, B0, B0, G2, G1, G1, G1, G1, G1, G1, G1, G1, G1, G1, G1, G1, G1, G1, G1, G1, G1, G1, G1, G2, B0, N0, N0],
-[N0, B0, B0, G2, G1, G1, G1, G1, G1, G1, G1, G2, G2, G2, G2, G2, G2, G2, G1, G1, G1, G1, G1, G1, G1, G2, B0, N0],
-[N0, B0, B0, G2, G1, G1, G1, G1, G1, G2, G2, G2, G2, G2, G2, G2, G2, G2, G2, G1, G1, G1, G1, G1, G1, G2, B0, N0],
-[N0, B0, B0, G2, G1, G1, G1, G1, G2, G2, G2, B1, G2, G2, G2, G2, G2, B1, G2, G2, G1, G1, G1, G1, G1, G2, B0, N0],
-[B0, B0, G2, G1, G1, G1, G1, G1, G2, G2, G2, G2, B1, G0, G0, G0, B1, G2, G2, G2, G2, G1, G1, G1, G1, G1, G2, B0],
-[B0, B0, G2, G1, G1, G1, G1, G2, G2, G2, G2, G2, G0, B1, G0, B1, G0, G2, G2, G2, G2, G1, G1, G1, G1, G1, G2, B0],
-[B0, B0, G2, G1, G1, G1, G1, G2, G2, G2, G2, G0, G0, G0, G0, G0, G0, G0, G2, G2, G2, G1, G1, G1, G1, G1, G2, B0],
-[B0, B0, G2, G1, G1, G1, G1, G2, G2, G2, B1, G0, G0, G0, G0, G0, G0, G0, B1, G2, G2, G1, G1, G1, G1, G1, G2, B0],
-[B0, B0, G2, G1, G1, G1, G1, G2, G2, G2, G2, G0, G0, G0, G0, G0, G0, G0, G2, G2, G2, G1, G1, G1, G1, G1, G2, B0],
-[B0, B0, G2, G1, G1, G1, G1, G2, G2, G2, G2, G2, G0, B1, G0, B1, G0, G2, G2, G2, G2, G1, G1, G1, G1, G1, G2, B0],
-[N0, B0, B0, G2, G1, G1, G1, G1, G2, G2, G2, G2, B1, G0, G0, G0, B1, G2, G2, G2, G2, G1, G1, G1, G1, G2, B0, N0],
-[N0, B0, B0, G2, G1, G1, G1, G1, G2, G2, G2, B1, G2, G2, G2, G2, G2, B1, G2, G2, G1, G1, G1, G1, G1, G2, B0, N0],
-[N0, B0, B0, G2, G1, G1, G1, G1, G1, G2, G2, G2, G2, G2, G2, G2, G2, G2, G2, G1, G1, G1, G1, G1, G1, G2, B0, N0],
-[N0, N0, B0, B0, G2, G1, G1, G1, G1, G1, G1, G2, G2, G2, G2, G2, G2, G2, G1, G1, G1, G1, G1, G1, G2, B0, N0, N0],
-[N0, N0, B0, B0, G2, G1, G1, G1, G1, G1, G1, G1, G1, G1, G1, G1, G1, G1, G1, G1, G1, G1, G1, G1, G2, B0, N0, N0],
-[N0, N0, N0, B0, B0, G2, G1, G1, G1, G1, G1, G1, G1, G1, G1, G1, G1, G1, G1, G1, G1, G1, G1, G2, B0, N0, N0, N0],
-[N0, N0, N0, N0, B0, B0, G2, G2, G1, G1, G1, G1, G1, G1, G1, G1, G1, G1, G1, G1, G1, G2, G2, B0, N0, N0, N0, N0],
-[N0, N0, N0, N0, N0, B0, B0, B0, G2, G2, G1, G1, G1, G1, G1, G1, G1, G1, G1, G2, G2, B0, B0, N0, N0, N0, N0, N0],
-[N0, N0, N0, N0, N0, N0, B0, B0, B0, B0, G2, G2, G1, G1, G1, G1, G1, G2, G2, B0, B0, N0, N0, N0, N0, N0, N0, N0],
-[N0, N0, N0, N0, N0, N0, N0, N0, B0, B0, B0, B0, G2, G2, G2, G2, G2, B0, B0, N0, N0, N0, N0, N0, N0, N0, N0, N0],
-[N0, N0, N0, N0, N0, N0, N0, N0, N0, N0, N0, B0, B0, B0, B0, B0, B0, N0, N0, N0, N0, N0, N0, N0, N0, N0, N0, N0],
+[N0, N0, N0, N0, N0, N0, N0, N0, N0, N0, G0, G0, G0, G0, G0, G0, G0, N0, N0, N0, N0, N0],
+[N0, N0, N0, N0, N0, N0, G0, G0, G0, G0, G0, G1, G1, G1, G1, G1, G0, G0, G0, N0, N0, N0],
+[N0, N0, N0, N0, G0, G0, G0, G1, G2, G2, G2, G2, G2, G2, G1, G1, G1, G1, G0, G0, N0, N0],
+[N0, N0, N0, G0, G0, G1, G1, G1, B0, G3, G3, B0, G3, G3, G1, G1, G1, G1, G1, G0, G0, N0],
+[N0, N0, G0, G0, G1, G1, G2, G2, G3, G3, G3, G3, G3, G3, G3, G2, G1, G1, G1, G0, B2, N0],
+[N0, G0, G0, G1, G1, G2, G2, G3, G3, G3, G3, G3, G3, G3, G3, G2, G1, G1, G1, G0, B2, B1],
+[G0, G0, G1, G1, G1, G2, B0, G3, G3, G3, G3, G3, G3, G3, B0, G1, G1, G1, G0, B2, B2, B1],
+[G0, G0, G1, G1, G1, G2, G2, G2, G3, G3, G3, G2, G2, G2, G2, G1, G1, G0, B2, B2, B1, B1],
+[G0, G0, G1, G1, G1, G1, B0, G2, G2, B0, G2, G2, G1, G1, G1, G1, G0, B2, B3, B2, B1, B1],
+[B2, G0, G0, G1, G1, G1, G1, G1, G1, G1, G1, G1, G1, G0, G0, G0, B2, B3, B2, B2, B1, B1],
+[B2, B2, G0, G0, G0, G1, G1, G1, G1, G1, G1, G0, G0, G0, B2, B2, B3, B3, B2, B2, B1, B1],
+[B1, B2, B2, B2, G0, G0, G0, G0, G0, G0, G0, G0, B2, B2, B3, B3, B3, B2, B2, B1, B1, N0],
+[N0, B1, B2, B3, B2, B2, B2, B2, B2, B2, B2, B2, B3, B3, B3, B3, B2, B2, B2, B1, B1, N0],
+[N0, B1, B2, B2, B4, B4, B3, B3, B3, B3, B3, B3, B3, B3, B2, B2, B2, B2, B1, B1, N0, N0],
+[N0, N0, B1, B1, B2, B2, B4, B3, B3, B4, B4, B3, B2, B2, B2, B2, B2, B1, B1, B1, N0, N0],
+[N0, N0, N0, B1, B1, B2, B2, B2, B2, B4, B4, B2, B2, B2, B2, B1, B1, B1, B1, N0, N0, N0],
+[N0, N0, N0, N0, B1, B1, B1, B1, B2, B2, B2, B2, B1, B1, B1, B1, B1, B1, N0, N0, N0, N0],
+[N0, N0, N0, N0, N0, B1, B1, B1, B1, B1, B1, B1, B1, B1, B1, B1, B1, N0, N0, N0, N0, N0],
+[N0, N0, N0, N0, N0, N0, N0, B1, B1, B1, B1, B1, B1, B1, B1, N0, N0, N0, N0, N0, N0, N0],
 ]
